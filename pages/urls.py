eebfceddf29c63147e3de71204a1d21688c3ba76
Atenas <atekapeko@gmail.com>
2024-06-29 15:16:52 -0400

    FIX: Routing and navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('contacto', views.contacto),
+    path('nosotros', views.nosotros),
+    path('productos', views.productos),
+    path('iniciar', views.login),
+    path('registrarse', views.register),
+    path('carrito', views.cart),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# Página de Inicio
 def home(request):
     return render(request, "index.html")
+
+# Página de Contacto
+def contacto(request):
+    return render(request, "contacto.html")
+
+# Página de Nosotros
+def nosotros(request):
+    return render(request, "nosotros.html")
+
+# Página de Productos
+def productos(request):
+    return render(request, "productos.html")
+
+# Página de Iniciar Sesión
+def login(request):
+    return render(request, "login.html")
+
+# Página de Registrarse
+def register(request):
+    return render(request, "register.html")
+
+# Página de Carrito
+def cart(request):
+    return render(request, "cart.html")
