704083980e46f2919a6736523c93ead8fae1ae58
Atenas <atekapeko@gmail.com>
2024-06-29 20:09:04 -0400

    FIX: Database models

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Producto, Usuario
-# Register your models here.
 
-admin.site.register(Producto)
-admin.site.register(Usuario)
+# Register your models here.
